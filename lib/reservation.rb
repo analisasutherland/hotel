3ad911326cd0cab718a9e51e6b5ec7d8cc938821
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-03-06 15:12:32 -0800

    Finished basic initialization for Reservation class with tests

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,11 +1,18 @@
 module Hotel
   class Reservation
 
-    attr_reader :res_id
+    attr_reader :res_id, :room_id, :check_in, :check_out
     # Want all reservations to have/store:
     # Reservation Id, Room Id, check_in, check_out
     def initialize(input)
       @res_id = input[:res_id]
+      @room_id = input[:room_id]
+      @check_in = input[:check_in]
+      @check_out = input[:check_out]
+
+      if @check_in.class != Date || @check_out.class != Date
+        raise ArgumentError.new("You must provide dates (got #{input[:check_in]} and #{input[:check_out]})")
+      end
     end
 
   end
--- specs/reservation_spec.rb
+++ specs/reservation_spec.rb
@@ -8,7 +8,7 @@ describe "Reservation" do
 
     @fake_reservation = {
       res_id: 1,
-      ROOM_ID: 03,
+      room_id: 03,
       check_in: start_date,
       check_out: check_out_date,
     }
@@ -28,6 +28,46 @@ describe "Reservation" do
       test_reservation.must_respond_to :res_id
 
       test_reservation.res_id.must_equal id
+    end
+
+    it "keeps track of room_id" do
+      test_room_id = @fake_reservation[:room_id]
+
+      test_reservation = Hotel::Reservation.new(@fake_reservation)
+
+      test_reservation.must_respond_to :room_id
+
+      test_reservation.room_id.must_equal test_room_id
+    end
+
+    it "keeps track of check in date" do
+      test_check_in = @fake_reservation[:check_in]
+
+      test_reservation = Hotel::Reservation.new(@fake_reservation)
+
+      test_reservation.must_respond_to :check_in
+
+      test_reservation.check_in.must_equal test_check_in
+    end
+
+    it "keeps track of check out date" do
+      test_check_out = @fake_reservation[:check_out]
+
+      test_reservation = Hotel::Reservation.new(@fake_reservation)
+
+      test_reservation.must_respond_to :check_out
+
+      test_reservation.check_out.must_equal test_check_out
+    end
+
+    it "will raise an error if check in and check out are not dates" do
+      @bad_date_reservation = {
+        res_id: 1,
+        room_id: 03,
+        check_in: nil,
+        check_out: nil,
+      }
+      proc{ Hotel::Reservation.new(@bad_date_reservation)}.must_raise ArgumentError
     end
   end
 end
